f82559d8abf3e3f818826d68cba0126ff6a614e5
MiniHarald <miniharald@gmail.com>
2019-11-12 16:55:54 +0100

    order fix

--- selectscript.js
+++ selectscript.js
@@ -1,20 +1,20 @@
-$('.shapes,.wheat, .pieType, .sweet1, .sweet2, .sweet3,.savoury1, .savoury2, .savoury3, .pie-customizer-dessert, .pie-customizer-food, .formular').hide();
+$(
+  ".shapes,.wheat, .pieType, .sweet1, .sweet2, .sweet3,.savoury1, .savoury2, .savoury3, .pie-customizer-dessert, .pie-customizer-food, .formular"
+).hide();
 
-
-
-//### current page 
+//### current page
 let currentPage;
 
 //### type of pie
 let pieType = 0;
 
-//#### selecterd 
+//#### selecterd
 
-//#### go to firs page 
-$('button').click(function(){
-    $('.preorderpage').hide();
-    $('.navBar').hide();
-    $('.shapes').show();
+//#### go to firs page
+$("button").click(function() {
+  $(".preorderpage").hide();
+  $(".navBar").hide();
+  $(".shapes").show();
 });
 //### select radio button
 // $(".box").click(function(){
@@ -22,179 +22,155 @@ $('button').click(function(){
 //     $("#circle").prop("checked", true);
 // });
 
-
 //##### change background-color
 
-$('.box').click(function(){
-    let selected = $(this).attr('id');
-    let selectedId = $("#checkbox-" + selected);
-    selectedId.prop("checked", true);
-    pieType = $(this).attr('type');
-    $(this).parent().find('.box').css("background-color", "");
-    $(this).css("background-color", "#afa0c5");
+$(".box").click(function() {
+  let selected = $(this).attr("id");
+  let selectedId = $("#checkbox-" + selected);
+  selectedId.prop("checked", true);
+  pieType = $(this).attr("type");
+  $(this)
+    .parent()
+    .find(".box")
+    .css("background-color", "");
+  $(this).css("background-color", "#afa0c5");
 });
 //### Pie Type Chosen
 
-function hidePieType(type){
-    if(type =='sweet'){
-        console.log('sweet selected')
-        $('.sweet1').show();
-        $('.pieType').hide();
-        
-    }
-    if(type == 'savoury'){
-        $('.pieType').hide();
-        $('.savoury1').show();
-    }
+function hidePieType(type) {
+  if (type == "sweet") {
+    console.log("sweet selected");
+    $(".sweet1").show();
+    $(".pieType").hide();
+  }
+  if (type == "savoury") {
+    $(".pieType").hide();
+    $(".savoury1").show();
+  }
 }
 
-//###### show next step 
+//###### show next step
 
-$('a').click(function(){
-    let input = $(this).attr('value')
-    //backward
-    currentPage = $(this).attr('type')
-    switch(currentPage){
-        case '0':
-            $('.shapes').hide();
-            $('.navBar').show();
-            $('.preorderpage').show();
-        break;
-        case '1':
-            $('.wheat').hide();
-            $('.shapes').show();
-        break;
-        case '2':
-            $('.wheat').show();
-            $('.pieType').hide();
-        break;
-        case '3':
-            $('.sweet1').hide();
-            $('.pieType').show();
-        break;
-        case '4':
-            $('.sweet2').hide();
-            $('.sweet1').show();
-        break;
-        case '5':
-            $('.sweet3').hide();
-            $('.sweet2').show();
-        break;
-        case '6':
-            $('.savoury1').hide();
-            $('.pieType').show();
-        break;
-        case '7':
-            $('.savoury2').hide();
-            $('.savoury1').show();
-        break;
-        case '8':
-            $('.savoury3').hide();
-            $('.savoury2').show();
-        break;
-    }
-    //forward
-    switch(input){
-        case 'shape':
-            $('.shapes').hide();
-            $('.wheat').show();
-        break;
-        case 'wheat':
-            $('.wheat').hide();
-            $('.pieType').show();
-        break;
-        case 'pieType':
-            hidePieType(pieType)
-        break;
-        case 'filling1':
-            $('.sweet1').hide();
-            $('.sweet2').show();
-        break;
-        case 'filling2':
-            $('.sweet2').hide();
-            $('.sweet3').show();
-        break;
-        case 'filling3':
-            $('.sweet3').hide();
-            $('.formular').show();
-            $('.foodPie').hide();
-            $('.pieTypes').hide();
+$("a").click(function() {
+  let input = $(this).attr("value");
+  //backward
+  currentPage = $(this).attr("type");
+  switch (currentPage) {
+    case "0":
+      $(".shapes").hide();
+      $(".navBar").show();
+      $(".preorderpage").show();
+      break;
+    case "1":
+      $(".wheat").hide();
+      $(".shapes").show();
+      break;
+    case "2":
+      $(".wheat").show();
+      $(".pieType").hide();
+      break;
+    case "3":
+      $(".sweet1").hide();
+      $(".pieType").show();
+      break;
+    case "4":
+      $(".sweet2").hide();
+      $(".sweet1").show();
+      break;
+    case "5":
+      $(".sweet3").hide();
+      $(".sweet2").show();
+      break;
+    case "6":
+      $(".savoury1").hide();
+      $(".pieType").show();
+      break;
+    case "7":
+      $(".savoury2").hide();
+      $(".savoury1").show();
+      break;
+    case "8":
+      $(".savoury3").hide();
+      $(".savoury2").show();
+      break;
+  }
+  //forward
+  switch (input) {
+    case "shape":
+      $(".shapes").hide();
+      $(".wheat").show();
+      break;
+    case "wheat":
+      $(".wheat").hide();
+      $(".pieType").show();
+      break;
+    case "pieType":
+      hidePieType(pieType);
+      break;
+    case "filling1":
+      $(".sweet1").hide();
+      $(".sweet2").show();
+      break;
+    case "filling2":
+      $(".sweet2").hide();
+      $(".sweet3").show();
+      break;
+    case "filling3":
+      $(".sweet3").hide();
+      $(".formular").show();
+      $(".foodPie").hide();
+      $(".pieTypes").hide();
 
+      break;
+    case "savoury1":
+      $(".savoury1").hide();
+      $(".savoury2").show();
+      break;
+    case "savoury2":
+      $(".savoury2").hide();
+      $(".savoury3").show();
+      break;
+    case "savoury3":
+      $(".savoury3").hide();
+      $(".formular").show();
+      $(".dessertPie").hide();
+      $(".pieTypes").hide();
 
-        break;
-        case 'savoury1':
-            $('.savoury1').hide();
-            $('.savoury2').show();
-        break;
-        case 'savoury2':
-            $('.savoury2').hide();
-            $('.savoury3').show();
-        break;
-        case 'savoury3':
-            $('.savoury3').hide();
-            $('.formular').show();
-            $('.dessertPie').hide();
-            $('.pieTypes').hide();
-
-        break;
-    }
+      break;
+  }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // let input = $(this).attr('value')
+// let input = $(this).attr('value')
 // let selected = $("."+ input)
 
 // let pieType = $(this).attr('type')
-// $('.outeroutercontainer').click( function() { 
+// $('.outeroutercontainer').click( function() {
 //     console.log('click')
 //     let idValue = $(this).attr('id');
-    
-   
-// }); 
+
+// });
 
 // $('#c,#s,#st,#h,.fa-hamburger,.fa-birthday-cake,#food,#cake').hide();
-// $('.fas').click( function() { 
-//     var inputValue = $(this).attr("value"); 
-//     var targetBox = $("." + inputValue); 
+// $('.fas').click( function() {
+//     var inputValue = $(this).attr("value");
+//     var targetBox = $("." + inputValue);
 
-//     $(".selectt").not(targetBox).hide(); 
+//     $(".selectt").not(targetBox).hide();
 //     console.log(firstChoice);
 //     $('.fa-hamburger, .fa-birthday-cake').show();
-//     $('#food, #cake').hide(); 
+//     $('#food, #cake').hide();
 //     $(targetBox).show();
 //     $(firstChoice).show();
-// }); 
+// });
 
 // $('#fo,#ca').click(function (){
-//     input = $(this).attr("value"); 
-//     target = $("." + input); 
+//     input = $(this).attr("value");
+//     target = $("." + input);
 //     $(target).show();
 //     console.log(firstChoice)
 //     $(firstChoice).show();
 // })
 
-
-
-
-
-
 // $('#circle').click(()=>{
 //     console.log('circle');
 // } );
-
